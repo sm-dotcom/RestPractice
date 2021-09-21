a954acd6270f7ab4bcda3ead52d0cbff1d9ed090
sm-dotcom <sarahmahmood@protonmail.com>
2021-09-21 15:34:50 +0500

    [Sarah] Main token updated file.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import time
 from selenium import webdriver
-<<<<<<< HEAD
-=======
 from selenium.webdriver.support.ui import Select
->>>>>>> [Sarah]Restapi Put and post request. Main token generated.
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -31,20 +28,25 @@ sign_in.click()
 # Click on API token
 browser.find_element_by_id('ctl00_ctrl_LeftMenuCloud1_hlnkAPIToken').click()
 
-# Enter name
-browser.find_element_by_id('tbName').send_keys('rest_api')
+# # Enter name
+# browser.find_element_by_id('tbName').send_keys('rest_api')
+#
+# # Integrator token
+# select_token = Select(browser.find_element_by_xpath('//*[@id="divSipTrunk"]/table/tbody/tr[1]/td[2]/table/tbody/tr[3]/td[2]/select'))
+# # select by visible text
+# select_token.select_by_visible_text('Integrator')
+#
+# #Generate Token
+# browser.find_element_by_xpath('//*[@id="divSipTrunk"]/table/tbody/tr[1]/td[2]/table/tbody/tr[4]/td[2]/button').click()
 
+# Get token --> copy
+table = browser.find_elements_by_xpath('//*[@id="gvAPIToken"]/tbody/tr')
+print(len(table))
 
-# Integrator token
-select_token = Select(browser.find_element_by_xpath('//*[@id="divSipTrunk"]/table/tbody/tr[1]/td[2]/table/tbody/tr[3]/td[2]/select'))
-# select by visible text
-select_token.select_by_visible_text('Integrator')
+for i in range:
 
-#Generate Token
-browser.find_element_by_xpath('//*[@id="divSipTrunk"]/table/tbody/tr[1]/td[2]/table/tbody/tr[4]/td[2]/button').click()
-
-
-
+# //*[@id="gvAPIToken"]/tbody/tr[1]/td[5]/div/img
+# //*[@id="gvAPIToken"]/tbody/tr[3]/td[5]/div/img
 
 browser.quit()
 # >>>>>>> [Sarah]Restapi Put and post request. Main token generated.
